3cb7510438b184ee122d7e45afec34d88d076bef
hamzaboularbah <boularbah59@gmail.com>
2019-07-27 16:18:11 +0100

    added some UI for the movies

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -2,15 +2,32 @@ import React from "react";
 
 let Movie = ({ movie, onLike, onDislike, onDelete }) => {
   return (
-    <div>
-      <h1>{movie.title}</h1>
-      <h2>{movie.category}</h2>
-      <span>{movie.likes}</span>
-      <button onClick={() => onLike(movie.id)}>Like</button>
-      <br />
-      <span>{movie.dislikes}</span>
-      <button onClick={() => onDislike(movie.id)}>Dislike</button>
-      <button onClick={() => onDelete(movie.id)}>Delete</button>
+    <div className="card">
+      {/* <figure className="image is-4by3" /> */}
+      <div className="card-content">
+        <div className="title is-4">{movie.title}</div>
+        <div className="title is-6">{movie.category}</div>
+      </div>
+      <footer className="card-footer">
+        <span className="card-footer-item" onClick={() => onLike(movie.id)}>
+          <i
+            className={movie.liked ? "fas fa-thumbs-up" : "far fa-thumbs-up"}
+          />
+          {movie.likes}
+        </span>
+
+        <span className="card-footer-item" onClick={() => onDislike(movie.id)}>
+          <i
+            className={
+              movie.disLiked ? "fas fa-thumbs-down" : "far fa-thumbs-down"
+            }
+          />
+          {movie.dislikes}
+        </span>
+        <span className="card-footer-item" onClick={() => onDelete(movie.id)}>
+          <i className="fas fa-trash-alt" />
+        </span>
+      </footer>
     </div>
   );
 };
